b0908a3a727bb4955785aa35dc64f60269b6ed1c
manifest <doslovno@gmail.com>
2019-01-07 14:51:18 +0300

    ProjectsView extends BaseView

--- app/src/main/java/com/elegion/test/behancer/ui/projects/ProjectsView.java
+++ app/src/main/java/com/elegion/test/behancer/ui/projects/ProjectsView.java
@@ -1,10 +1,11 @@
 package com.elegion.test.behancer.ui.projects;
 
+import com.elegion.test.behancer.common.BaseView;
 import com.elegion.test.behancer.data.model.project.Project;
 
 import java.util.List;
 
-public interface ProjectsView {
+public interface ProjectsView extends BaseView {
 
     void showProjects(List<Project> projects);
 
